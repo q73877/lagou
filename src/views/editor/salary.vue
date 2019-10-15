1c7a2b0886aabb11ab4871651fa2bf949c829bf3
Your Name <you@example.com>
2019-10-15 15:17:44 +0800

    147

--- src/views/editor/salary.vue
+++ src/views/editor/salary.vue
@@ -13,12 +13,75 @@
         </span>
       </div>
       
-      <ul class="list"></ul>
+      <ul class="list">
+        <li class="activeable item">没有要求</li>
+        <li class="activeable item">2k以下</li>
+        <li class="activeable item">2k-5k</li>
+        <li class="activeable item">5k-10k</li>
+        <li class="activeable item">10k-15k</li>
+        <li class="activeable item">15k-25k</li>
+        <li class="activeable item">25k-50k</li>
+        <li class="activeable item">50k以上</li>
+      </ul>
     </div>
   </div>
 </template>
 <style lang="scss">
   .page-salary{
-    
+    .info{
+      background:url(https://www.lgstatic.com/images/mobile/asset/common/img/back-info.png) no-repeat;
+      background-size: 320px 125px;
+      height:125px;
+      color:#00b38a;
+      font-size: 20px;
+      text-align: center;
+      border-bottom: 1px solid #e8e8e8; 
+      .text{
+        display: inline-block;
+        position: relative;
+        height:25px;
+        top:50px;
+      }
+      .up{
+        top:35px;
+        left: -22px;
+      }
+      .down{
+        top:40xp;
+        left:23px;
+      }
+      .left,.right{
+        display: inline-block;
+        width:13px;
+        height:10.5px;
+        position: absolute;
+      }
+      .left{
+        left:-20px;
+        top:-2px;
+        background:url(https://www.lgstatic.com/images/mobile/asset/common/img/icon.png) no-repeat -102.5px -4px;
+        background-size: 250px 250px;
+      }
+      .right{
+        left:-20px;
+        top:-2px;
+        background:url(https://www.lgstatic.com/images/mobile/asset/common/img/icon.png) no-repeat -119.5px -4px;
+        background-size: 250px 250px;
+      }      
+    }
+    .list{
+        margin-left: 15px;
+        .item{
+          height:44px;
+          line-height: 44px;
+          color:#666;
+          font-size: 15px;
+          border-bottom: 1px solid #e8e8e8;
+          padding: 0 15px 0 10px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+      }
   }
 </style>
--- src/views/film/index.vue
+++ src/views/film/index.vue
@@ -115,11 +115,7 @@
 
       <div class="fix_btn_group">
         <div
-          class="deliver deliver_resume rows_bar"
-          data-lg-tj-id="19v6"
-          data-lg-tj-no="0020"
-          data-lg-tj-cid="6460970"
-        >投递简历</div>
+          class="deliver deliver_resume rows_bar">投递简历</div>
       </div>
     </div>
   </div>
