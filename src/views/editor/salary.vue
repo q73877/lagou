b6b09c20c2a888bf5e033c5271381f741a9705cb
Your Name <you@example.com>
2019-10-15 11:44:36 +0800

    123

--- src/views/editor/salary.vue
+++ src/views/editor/salary.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-
+  <div class="page-salary">
+    <div id="content">
+      <div class="info">
+        <span class="text up">
+            <em class="left"></em>
+            你值得更好的生活，
+        </span>
+        <br />
+        <span class="text down">
+          告诉我你期望的薪水。
+          <em class="right"></em>
+        </span>
+      </div>
+      
+      <ul class="list"></ul>
+    </div>
   </div>
 </template>
+<style lang="scss">
+  .page-salary{
+    
+  }
+</style>
